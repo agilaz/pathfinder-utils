4b313f98be5d1294af3abaabda8417f8f1da35a0
agilaz <erk700@gmail.com>
2017-02-25 14:57:21 -0500

    Added basic ability routes

---
+++ app/controllers/abilities_controller.rb
@@ -0,0 +1,52 @@
+class AbilitiesController < ApplicationController
+  def index
+    @abilities = Ability.all
+  end
+
+  def show
+    @ability = Ability.find(params[:id])
+  end
+
+  def new
+    @ability = Ability.new
+  end
+
+  def create
+    this_param = ability_params
+    this_param[:uses_left] = this_param[:max_uses]
+    @ability = Ability.new(this_param)
+    if @ability.save
+      redirect_to(root_path)
+    else
+      render('new')
+    end
+  end
+
+
+  def edit
+    @ability = Ability.find(params[:id])
+  end
+
+  def update
+    #TODO add check for uses_left > max_uses
+    @ability = Ability.find(params[:id])
+    if @ability.update_attributes(ability_params)
+      redirect_to(abilities_path)
+    else
+      render('edit')
+    end
+  end
+
+  def delete
+    @ability = Ability.find(params[:id])
+  end
+
+  def destroy
+    Ability.find(params[:id]).destroy
+    redirect_to(abilities_path)
+  end
+
+  def ability_params
+    params.require(:ability).permit(:name, :max_uses)
+  end
+end
