c45746414f8a1a918557cabbfcdd2486ba67d2e8
Joseph Buonocore <jbuonoco@stevens.edu>
2017-02-25 23:34:51 -0500

    Reformat prepared_spells

--- app/controllers/prepared_spells_controller.rb
+++ app/controllers/prepared_spells_controller.rb
@@ -2,18 +2,15 @@ class PreparedSpellsController < ApplicationController
   def index
     character = Character.find(session[:character_id])
     @spells_allowed = spells_per_day(character)
-    @spells = Hash.new
-    for i in 0..@spells_allowed.length
-      unless @spells_allowed[i].nil?
-        @spells[i]=PreparedSpell.all.where(:level => i).order('name ASC')
-      end
-    end
+    @spells = get_spells(@spells_allowed)
   end
 
   def new
     @prepared_spell = PreparedSpell.new
     character = Character.find(session[:character_id])
     @all_spells = Spell.all.where(character.char_class.to_sym => params[:level]).order('name ASC')
+    @spells_allowed = spells_per_day(character)
+    @spells = get_spells(@spells_allowed)
   end
 
   def create
@@ -27,5 +24,14 @@ class PreparedSpellsController < ApplicationController
     end
   end
 
+  def get_spells(spells_allowed)
+    spells = Hash.new
+    for i in 0..spells_allowed.length
+      unless spells_allowed[i].nil?
+        spells[i]=PreparedSpell.all.where(:level => i).order('name ASC')
+      end
+    end
+    spells
+  end
 
 end
